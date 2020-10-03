3056a5159df5e33f454692555790ba1618da7a70
xjyssg <jiayuexue30@gmail.com>
2020-10-03 10:06:35 +0800

    7.8

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -20,14 +20,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+  
+  const create = async resource => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
